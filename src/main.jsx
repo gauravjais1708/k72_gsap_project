7e8158488556b1da76ddc77bc3446b0e86c93bea
Gaurav jaiswal <gauravjayswal17082002@gmail.com>
2025-11-09 12:02:51 +0530

    fixed bugs

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import Stairs from './components/common/Stairs.jsx'
+import Stairs from './components/common/stairs.jsx'
 import NavContext from './context/NavContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
